71ce9139f1791f2f92d028c2fb6721e40ab8075f
Doom_hopes <diadia1911@ukr.net>
2020-11-14 02:08:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText etAuthor;
+    private TextView tvChatBox;
+    private TextView tvAuthor;
+    private EditText etMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etAuthor = findViewById(R.id.editTextAuthor);
+        tvChatBox = findViewById(R.id.textViewChatBox);
+        tvAuthor = findViewById(R.id.textViewAuthor);
+        etMessage = findViewById(R.id.editTextPersonAuthor);
+
+        tvAuthor.setText(etAuthor.getText() + ":");
+        tvChatBox.setMovementMethod(new ScrollingMovementMethod());
+    }
+
+    public void onClickSetAuthor(View view) {
+        CharSequence authorName = etAuthor.getText();
+        if(authorName.equals("")){
+            Toast.makeText(this,"Please, enter your name", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        tvAuthor.setText(authorName + ":");
+    }
+
+    public void sendMessage(View view) {
+        String message = etMessage.getText().toString();
+        if(message.equals("")){
+            Toast.makeText(this,"Please, enter message", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        tvChatBox.append(tvAuthor.getText() + " - " + message + "\n");
+        etMessage.setText("");
+    }
+}
---
+++ app/src/main/java/com/example/chat/Message.java
@@ -0,0 +1,87 @@
+package com.example.chat;
+
+//ORM
+
+import android.annotation.SuppressLint;
+
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.SimpleTimeZone;
+
+public class Message {
+
+    private static SimpleDateFormat sqlFormat = new SimpleDateFormat("YYYY-MM-dd hh:mm:ss");
+
+    //Model
+    private String text;
+    private String author;
+    private Date moment;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Date moment) {
+        this.moment = moment;
+    }
+
+    //Interface
+    public Message(String text, String author, Date moment) {
+        this.text = text;
+        this.author = author;
+        this.moment = moment;
+    }
+
+    public Message(String text, String author) {
+        this.text = text;
+        this.author = author;
+        this.moment = new Date();
+    }
+
+    @Override
+    public String toString(){
+        return this.author + " - " + this.text;
+    }
+
+    //Fabric
+    public static Message fromJSONString(String json){
+        // {"id":22, "author":"Name", "text":"Message", "moment":"2020-11-11 12:12:12"}
+        try{
+            JSONObject jo = new JSONObject(json);
+            return new Message(jo.getString("author"), jo.getString("text"),
+                    sqlFormat.parse(jo.getString("moment")));
+        }
+        catch (Exception ignored){
+            return null;
+        }
+    }
+
+    public static Message fromJSONObject(JSONObject jo){
+        // {"id":22, "author":"Name", "text":"Message", "moment":"2020-11-11 12:12:12"}
+        try{
+            return new Message(jo.getString("author"), jo.getString("text"),
+                    sqlFormat.parse(jo.getString("moment")));
+        }
+        catch (Exception ignored){
+            return null;
+        }
+    }
+}
